0252267c97081808e5e39d8070d6133b7db17822
hackgames13 <159486747+hackgames13@users.noreply.github.com>
2025-04-25 23:37:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-const Discord = require('discord.js'); //import client from discord
 const keep_alive = require('./keep_alive.js')
+const http = require('http');
+const api_index = require('./api_index.js');
+const voice = require('./voice.js');
+const fs = require('fs');
+const path = require('path');
 
-const client = new Discord.Client();
+const server = http.createServer((req, res) => {
+  // Remove query string from the URL
+  let filePath = './src' + req.url.split('?')[0]; // Only take the part before the "?"
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml'
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile('./404.html', (err, notFoundContent) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(notFoundContent || '404 Not Found', 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${error.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 
-client.on('message', msg => {
-    // check if message isn't from us
-    if (msg.author == client.user) {
-      return;
-    }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-    else {
-        msg.reply(msg.content);
-        console.log(msg.content);
-    }
+server.listen(5000, '0.0.0.0', () => {
+  console.log('Server running at http://0.0.0.0:5000/');
 });
-
-client.login(process.env.TOKEN); //login bot using token
